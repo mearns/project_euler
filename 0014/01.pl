b179557c849f75821b90bb20ab03dec52503b84c
Brian Mearns <bmearns@ieee.org>
2015-01-21 15:04:56 -0500

    Reversed order of conditions on odd/even test to avoid equality test.

--- 0014/01.pl
+++ 0014/01.pl
@@ -19,11 +19,11 @@ sub collatz {
 
     #print "$n ";
     until ($n == 1) {
-        if ($n % 2 == 0) {
-            $n /= 2;
+        if ($n % 2) {
+            $n = 3*$n + 1;
         }
         else {
-            $n = 3*$n + 1;
+            $n /= 2;
         }
         #print " -> $n";
 
